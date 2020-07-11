cc319035e21ea71bf16101c21a7803579fdf6e75
Dmitrijs Kaminskis <dmitrijs.kaminskis@gmail.com>
2020-07-11 17:40:35 +0300

    updated manage.py for heroku deployment

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from backend.src.api import create_app
 from backend.src.database.models import db
 
+MIGRATION_DIR = os.path.join('backend', 'src', 'migrations')
+
 app = create_app()
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, directory=MIGRATION_DIR)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
