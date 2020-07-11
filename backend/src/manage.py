00081e1e20cf20dada6450ee4a0a4b38455819ff
Dmitrijs Kaminskis <dmitrijs.kaminskis@gmail.com>
2020-07-11 19:26:25 +0300

    changed the location of heroku files

--- backend/src/manage.py
+++ backend/src/manage.py
@@ -1,13 +1,11 @@
-import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from backend.src.api import app
 from backend.src.database.models import db
 
-MIGRATION_DIR = os.path.join('backend', 'src', 'migrations')
 
-migrate = Migrate(app, db, directory=MIGRATION_DIR)
+migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
