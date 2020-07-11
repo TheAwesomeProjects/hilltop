0296726ad1eef2dc817e9152d3b2918b19be179f
Dmitrijs Kaminskis <dmitrijs.kaminskis@gmail.com>
2020-07-11 18:45:46 +0300

    updated setup.sh for heroku deployment

--- setup.sh
+++ setup.sh
@@ -1,4 +1,1 @@
-AUTH0_DOMAIN=awesome-auth.eu.auth0.com
-ALGORITHMS=RS256
-API_AUDIENCE=hilltop
-DATABASE_URL=postgres://wwmmsmiqlbeubh:439863f97fb328c93225742ada9ae9c7afa232e06c9d327497c0bb45600ac6b1@ec2-54-234-28-165.compute-1.amazonaws.com:5432/dqmiqe1butmcd
+export LANG=${LANG:-en_US.UTF-8}
